dec3cc80cbdbd3542758a6b65ef81f8bbb3b5268
Himo <jerameel.himo2001@gmail.com>
2023-10-15 15:26:13 +0800

    added migration and models for carousel

---
+++ database/migrations/2023_10_15_063755_create_carousel_item_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('carousel_item', function (Blueprint $table) {
+            $table->id('carousel_item_id');
+            $table->string('carousel_name')->nullable();
+            $table->string('image_path');
+            $table->string('description')->nullable();
+            $table->timestamps();
+        });
+        Schema::table('carousel_item', function (Blueprint $table) {
+            $table->unsignedBigInteger('user_id');
+         
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('carousel_item');
+    }
+};
